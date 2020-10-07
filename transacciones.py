8c537240baff024ac96fc5751ac157adce82c403
unknown <Admin@PE-HO-350.PERIFERIAGROUP.MS>
2020-10-07 10:19:27 -0500

    Conexion BD OKk

--- transacciones.py
+++ transacciones.py
@@ -1,26 +1,28 @@
 import psycopg2
 from config import *
 
-conexion = psycopg2.connect(user=USER, password=PASSWORD, host=HOST, port=PORT, database=DB)
+conexion = psycopg2.connect(user="postgres", password="Periferia2020", host="127.0.0.1", port="5432", database="postgres")
 
 # conexion.autocommit = True # Solo para pruebas por lo general
+#El trybloque le permite probar un bloque de código en busca de errores.
 try:
     cursor = conexion.cursor()
-    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)' #%s es un comodin, que permite insertar valores dinánimocs
-    valores = ('Mario', 'Gomez', 'mgomez@mail.com')
+    sentencia = 'INSERT INTO public.insertar_datos(nombre, apellido, email) VALUES(%s, %s, %s)' #%s es un comodin, que permite insertar valores dinánimocs
+    valores = ('Ithan', 'Daniel', 'danielespitia1@outlook.com')
     cursor.execute(sentencia, valores)
 
-    sentencia = 'UPDATE persona SET nombre = %s, apellido = %s, email = %s WHERE id_persona = %s' #%s es un comodin, que permite insertar valores dinánimcos
-    valores = ('Juan1', 'Perezoso2', 'jperez2@mail.com', 1)
+    sentencia = 'UPDATE public.insertar_datos SET nombre = %s, apellido = %s, email = %s WHERE id = %s' #%s es un comodin, que permite insertar valores dinánimcos
+    valores = ('Juan1', 'Perezoso2', 'jperez2@mail.com', 18)
     cursor.execute(sentencia, valores)
 
     # Guardamos la información en la base de datos
     conexion.commit()
+#El exceptbloque le permite manejar el error.
 except Exception as e:
     # Rollback da marcha atras a todas las operaciones SQL pendientes
     conexion.rollback()
     print(f"Ocurrió un error en la transacción: {e}")
-
+#El finallybloque le permite ejecutar código, independientemente del resultado de los bloques try y except.
 finally:
     cursor.close()
     conexion.close()
